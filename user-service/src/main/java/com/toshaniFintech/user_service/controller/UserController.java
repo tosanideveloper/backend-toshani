a19c2ca53c0a4150119998f966ca4e7d2c5619c5
RudraNarayanSethy <rudranarayan@socialtalentagency.com>
2026-03-31 01:02:59 +0530

    test changes

--- user-service/src/main/java/com/toshaniFintech/user_service/controller/UserController.java
+++ user-service/src/main/java/com/toshaniFintech/user_service/controller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users_info")
+@RequestMapping("/users")
 public class UserController {
 
     @GetMapping
