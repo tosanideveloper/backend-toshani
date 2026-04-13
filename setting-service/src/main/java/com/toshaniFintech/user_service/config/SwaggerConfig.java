c3dbee8cd8f14b26925e9797c5805b282e55d64f
RudraNarayanSethy <rudranarayan@socialtalentagency.com>
2026-04-13 22:46:03 +0530

    Fixed cors issues

--- setting-service/src/main/java/com/toshaniFintech/user_service/config/SwaggerConfig.java
+++ setting-service/src/main/java/com/toshaniFintech/user_service/config/SwaggerConfig.java
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -13,6 +16,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(new Server().url("https://testapi.toshani.in/settings")))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth",
--- user-service/src/main/java/com/toshaniFintech/user_service/config/SecurityConfig.java
+++ user-service/src/main/java/com/toshaniFintech/user_service/config/SecurityConfig.java
@@ -29,7 +29,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:3000",
                 "http://localhost:8080",
-                "http://localhost:8082"
+                "http://localhost:8082",
+                "https://testapi.toshani.in"
         ));
         configuration.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "DELETE", "OPTIONS"
