e86724a63f4c865de3c9c56295db1f358c103bcf
Taraprasad Panda <taraprasad.panda@toshani.in>
2026-04-14 11:46:09 +0530

    Amc Report Get all Reports in Post Mapping.

--- setting-service/src/main/java/com/toshaniFintech/user_service/service/AmcReportService.java
+++ setting-service/src/main/java/com/toshaniFintech/user_service/service/AmcReportService.java
@@ -1,11 +1,10 @@
 package com.toshaniFintech.user_service.service;
 
-import com.toshaniFintech.user_service.dto.response.AmcReportResponse;
 import com.toshaniFintech.user_service.model.AmcReportModel;
-import com.toshaniFintech.user_service.model.ApiPartnerServiceModel;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 
 public interface AmcReportService {
-    List<AmcReportModel> getAllAmcReport();
+    List<AmcReportModel> getAllAmcReport(PageRequest pageRequest);
 }
